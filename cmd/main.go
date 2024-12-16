c6a71e7c435733218afd16763f5eafbf2261f04b
Timur <Tima_mamedov_1991@mail.ru>
2024-12-16 13:15:30 +0300

    Create api Todo

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,15 @@
 package main
 
+import (
+	"github.com/TimurMamdov1991/todo-app"
+	"github.com/TimurMamdov1991/todo-app/pkg/handler"
+	"log"
+)
+
 func main() {
+	handlers := new(handler.Handler)
 	srv := new(todo.Server)
+	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
+		log.Fatalf("error occured while running http server: %s", err.Error())
+	}
 }

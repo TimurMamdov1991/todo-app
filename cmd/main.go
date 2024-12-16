a95f658e6e6f3f408111087ffcde58f970429a1d
Timur <Tima_mamedov_1991@mail.ru>
2024-12-16 13:40:21 +0300

    Dependency Injection

--- cmd/main.go
+++ cmd/main.go
@@ -3,11 +3,16 @@ package main
 import (
 	"github.com/TimurMamdov1991/todo-app"
 	"github.com/TimurMamdov1991/todo-app/pkg/handler"
+	"github.com/TimurMamdov1991/todo-app/pkg/repository"
+	service "github.com/TimurMamdov1991/todo-app/pkg/service"
 	"log"
 )
 
 func main() {
-	handlers := new(handler.Handler)
+	repos := repository.NewRepository()
+	services := service.NewService(repos)
+	handlers := handler.NewHandler(services)
+
 	srv := new(todo.Server)
 	if err := srv.Run("8888", handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while running http server: %s", err.Error())
